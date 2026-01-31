31c05cb731f29f24517d4ee1e0b449d9bbfe4c00
San Ja Nar (Selena) <selena141ss@gmail.com>
2026-01-31 12:10:14 +0700

    Create User.java

---
+++ backend/src/main/java/com/brainforrent/model/User.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+// Base Class
+abstract class User {
+    protected String userId;
+    protected String name;
+    protected String email;
+    protected String role;
+
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void login() { System.out.println(name + " logged in."); }
+    public void logout() { System.out.println(name + " logged out."); }
+}
